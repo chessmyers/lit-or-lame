001e7be5b2913ccee0036700505a492e2ac52590
Christopher Myers <chessmyers@aol.com>
2022-12-23 17:46:53 -0800

    Add build files

---
+++ build/precache-manifest.8730faaf7d6c5f7cc6912fe7402c0d97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da59d906fa375e6d3aa769f4de7e6578",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f235a91a6055b56da35",
+    "url": "/static/css/main.e222c774.chunk.css"
+  },
+  {
+    "revision": "ec6718cb7894f033d258",
+    "url": "/static/js/2.472399b3.chunk.js"
+  },
+  {
+    "revision": "6f235a91a6055b56da35",
+    "url": "/static/js/main.e06a5447.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
